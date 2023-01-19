5f8a637cf626679ab66873c2c65dc63024f6c9c4
xxradar <xxradar@radarhack.com>
2023-01-19 16:09:43 +0000

    My new feature 0.2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");  
 http.createServer(function (request, response) {  
        response.writeHead(200, {'Content-Type': 'text/plain'});  
-       response.end('Hello World in 2023\n');  
+       response.end('Hello World in 2023 and looking fwd to 2024\n');  
     }).listen(8081);  
 console.log('Server running at http://127.0.0.1:8081/');  
